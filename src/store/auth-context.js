0132e53273bab7bfde54cab8d676513fb9d4297d
diegomunozme <diego.munoz.me@gmail.com>
2022-07-11 05:43:34 -0700

    need to finish lesson 125

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-// Authcontext here is not directly a componenet, but an 
+// Authcontext here is not directly a componenet, but an
 //  object that contains a components
 const AuthContext = React.createContext({
-    isLoggedIn: false
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 });
+
+/*
+    We can also manage the isLoggedIn state from 
+    this auth context file, 
+*/
+const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+    const logoutHandler = () =>{
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler =() =>{
+        localStorage.setItem("isLoggedIn", "1");
+        setIsLoggedIn(true);
+    }
+  return <AuthContext.Provider> {props.children} </AuthContext.Provider>;
+};
 /*
  We will now need to import this context object and wrap it around all the most 
  top level components we wish to use it in 
